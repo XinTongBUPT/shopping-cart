e409a7996156c98b30cf6661491430c5c57c2463
Xin Tong <xintongbupt@outlook.com>
2019-05-01 22:54:10 -0500

    update

---
+++ build/precache-manifest.7d5b4b32fd8204fbf44e77ae2493592c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b698d6dbfaf10d84d983440e2b5a433",
+    "url": "/static/media/100_2.9b698d6d.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11a63e5e61611b569528",
+    "url": "/static/js/main.40121c48.chunk.js"
+  },
+  {
+    "revision": "5758ed1211e3aadf9f4c",
+    "url": "/static/js/2.19db6901.chunk.js"
+  },
+  {
+    "revision": "11a63e5e61611b569528",
+    "url": "/static/css/main.ae36e356.chunk.css"
+  },
+  {
+    "revision": "4c544cd4c9b74c9f1a4fef7caff8e7d6",
+    "url": "/index.html"
+  }
+];
